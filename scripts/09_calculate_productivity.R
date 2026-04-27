a70138b235a2cc55f875d4551c991ac6538b26e5
Camilla Stefanini <47189157+castef@users.noreply.github.com>
2026-04-27 18:33:51 +0200

    Add files via upload

---
+++ scripts/09_calculate_productivity.R
@@ -0,0 +1,161 @@
+
+### this script
+# 1. read the processed demo, fit and quanti dataset, filter for adults
+# 2. summarize fit and quanti from individual to stand level (one line per replicate)
+# 3. merges the 3 datasets
+# 4. computes productivity (NW), productivity relative to the final productivity and relative to the neutral productivity for eahc year and simulation
+# 5. save the result into Final_dataset_replicate_level.RDS
+
+
+library(data.table)
+library(stringr)
+library(ggplot2)
+library(terra)
+library(ggh4x)
+library(ggpubr)
+
+res_path <- "C:/Users/stefanin/Dropbox/WSL_PhD/Projects/Hybridization2/output/analysis"
+fig_path <- "C:/Users/stefanin/Dropbox/WSL_PhD/Projects/Hybridization2/Figures_manuscript"
+
+############## combine data and calculate productivity ##########
+
+dt1 <- readRDS(file.path(res_path, "Demo_data_processed.RDS"))
+dt2 <- readRDS(file.path(res_path, "Fit_data_processed.RDS")) ## individual-level data
+dt3 <- readRDS(file.path(res_path, "Quanti_data_processed.RDS"))  ## individual-level data
+
+
+## correct runs!!!
+dt2[configuration == "No introduction", run := "r01"]
+dt3[configuration == "No introduction", run := "r01"]
+
+# create run+ replicate id and supset for adults
+dt1$replicate2 <- paste0(dt1$run, "_", dt1$replicate)
+dt2$replicate2 <- paste0(dt2$run, "_", dt2$replicate)
+dt3$replicate2 <- paste0(dt3$run, "_", dt3$replicate)
+
+dt1_ad <- dt1[age_class == "Stage 3"]
+dt2_ad <- dt2[age_class == 3]
+dt3_ad <- dt3[age_class == 3]
+
+# median fitness across the landscape ---> from individual level to stand level
+dt2_median <- dt2_ad[, .(
+  W = median(W)
+), by = .(configuration, proportion_orientalis,selection_type, selection_strength,year, age_class, replicate2)]
+
+
+# hybrid proportion per replicate ---> from individual level to stand level
+dt3_prop <- dt3_ad[, .(
+  prop_hybrid = mean(P1 >= -0.9 & P1 <= 0.9)
+), by = .(configuration, proportion_orientalis,selection_type, selection_strength, year, age_class, replicate2)]
+
+
+# merge 
+dt_merged <- Reduce(function(x, y) merge(x, y,
+                                         by = c("configuration", "proportion_orientalis","selection_type", "selection_strength","replicate2", "year")),
+                    list(
+                      dt1_ad[, .(configuration, proportion_orientalis,selection_type, selection_strength,replicate2,year, N_stage)],
+                      dt2_median[, .(configuration, proportion_orientalis,selection_type, selection_strength,replicate2,year, W = W)],
+                      dt3_prop[, .(configuration, proportion_orientalis,selection_type, selection_strength,replicate2,year, Hyb_proportion = prop_hybrid)] 
+                    ))
+
+
+# calculate productivity
+dt_merged[, NW := N_stage * W]
+
+## check NW  ---> save for supplementary
+ggplot(dt_merged,
+       aes(year,  NW,
+           group = interaction(configuration, replicate2) ))+  
+  geom_line() +
+  labs(x = "Year")+
+  facet_nested(proportion_orientalis+ configuration  ~ selection_type + selection_strength) +
+  theme_bw()+ theme(strip.text.y = element_text(angle=0))
+
+ggsave(
+  paste0(fig_path,"/NW_trend.png"),
+  plot = last_plot(),   # or assign your plot to an object and use plot = p
+  width = 20,
+  height = 10,
+  units = "in"
+)
+
+
+
+# extract the final value of NW at equilibrium (as the reference == 100% productivity)
+dt_final_NW <- dt_merged[, .SD[which.max(year)], 
+                       by = .(configuration, proportion_orientalis,selection_type, selection_strength,replicate2)]
+
+dt_final_NW <- dt_final_NW[, .(
+  configuration, proportion_orientalis,selection_type, selection_strength,replicate2,NW_final = NW
+)]
+
+
+# attach back
+dt_merged2 <- merge(dt_merged, dt_final_NW,
+                 by = c("configuration", "proportion_orientalis","selection_type", "selection_strength","replicate2"))
+
+## calculate NW relative to the final NW at equilibrium (= within scneario dynamics = how fast the system reaches its own equilibrium)
+dt_merged2[, NW_rel := NW / NW_final]
+
+## check NW_rel
+ggplot(dt_merged2,
+       aes(year,  NW_rel,
+           group = interaction(configuration, replicate2) ))+  
+  geom_line() +
+  labs(x = "Year")+
+  facet_nested(proportion_orientalis+ configuration  ~ selection_type + selection_strength, scales = "free_y") +
+  theme_bw()
+
+
+## calculate NW (and hybrid proportion) relative to the neutral baseline ( = effect of selection relative to neutral expectation)
+# extract NW neutral baseline and merge it back
+dt_neutral_NW <- dt_merged2[
+  selection_type == "Neutral",   
+  .(configuration, proportion_orientalis, replicate2,year, NW_neutral = NW)
+]
+
+dt_neutral_hyb <- dt_merged2[
+  selection_type == "Neutral",   
+  .(configuration, proportion_orientalis, replicate2,year, Hyb_neutral = Hyb_proportion)
+]
+
+dt_merged3 <- merge(dt_merged2,dt_neutral_NW,
+                   by = c("configuration", "proportion_orientalis", "replicate2", "year"),
+                   all.x = TRUE
+)
+
+dt_merged4 <- merge(dt_merged3,dt_neutral_hyb,
+                   by = c("configuration", "proportion_orientalis", "replicate2","year"),
+                   all.x = TRUE
+)
+
+
+# compute corrected NW and clean from edge cases
+dt_merged4[, NW_corr_neutral := NW / NW_neutral]
+dt_merged4[, Hyb_corr_neutral := Hyb_proportion / Hyb_neutral]
+
+
+## check scaled values
+ggplot(dt_merged4,
+       aes(year,  NW_corr_neutral,
+           group = interaction(configuration, replicate2) ))+  
+  geom_line() +
+  labs(x = "Year")+
+  facet_nested(proportion_orientalis+ configuration  ~ selection_type + selection_strength) +
+  theme_bw()
+
+ggplot(dt_merged4,
+       aes(year,  Hyb_corr_neutral,
+           group = interaction(configuration, replicate2) ))+  
+  geom_line() +
+  labs(x = "Year")+
+  facet_nested(proportion_orientalis+ configuration  ~ selection_type + selection_strength) +
+  theme_bw()
+
+
+rm(dt1, dt2, dt3, dt1_ad, dt2_ad, dt3_ad)
+rm(dt_merged, dt_merged2, dt_merged3)
+
+## save dataset
+saveRDS(dt_merged4, file.path(res_path, "Final_dataset_replicate_level.RDS"))
+
